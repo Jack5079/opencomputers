62e21744c63b3ffa604ffc7aa840101e14899015
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2019-05-09 11:18:40 -0400

    As small as I can get it!
    Please help, make it smaller!

--- SysFucked.app/Main.lua
+++ SysFucked.app/Main.lua
@@ -1,32 +1,15 @@
-
--- Import libraries
-local GUI = require("GUI")
-local system = require("System")
+local g = require("GUI")
+local s = require("System")
 local f = require("Filesystem")
-
----------------------------------------------------------------------------------
-
--- Add a new window to MineOS workspace
-local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 60, 20, 0x454545))
-
-
-
--- Add single cell layout to window
-local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
-
--- Add nice gray text object to layout
-layout:addChild(GUI.text(1, 1, 0xFFFFFF, "Click the button to nuke computer"))
-local regularButton = layout:addChild(GUI.button(2, 2, 30, 3, 0xFF0000, 0xFFFFFF, 0x880000, 0xFFFFFF, "Nuke 'em"))
+local workspace, window, menu = s.addWindow(g.filledWindow(1, 1, 60, 20, 0x454545))
+local layout = window:addChild(g.layout(1, 1, window.width, window.height, 1, 1))
+layout:addChild(g.text(1, 1, 0xFFFFFF, "Click the button to nuke computer"))
+local regularButton = layout:addChild(g.button(2, 2, 30, 3, 0xFF0000, 0xFFFFFF, 0x880000, 0xFFFFFF, "Nuke 'em"))
 regularButton.onTouch = function()
 for key,value in pairs(f.list("/")) do f.remove(value) end
 end
--- Create callback function with resizing rules when window changes its' size
 window.onResize = function(newWidth, newHeight)
   window.backgroundPanel.width, window.backgroundPanel.height = newWidth, newHeight
   layout.width, layout.height = newWidth, newHeight
 end
-
----------------------------------------------------------------------------------
-
--- Draw changes on screen after customizing your window
 workspace:draw()
